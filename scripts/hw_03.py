f93fdd6b0525d6b17310259a68f36244bde7fd14
JaySharmaSdsu <jsharma3962@sdsu.edu>
2023-03-09 21:50:24 -0800

    Hi please find my final commit for the third homework!

--- scripts/hw_03.py
+++ scripts/hw_03.py
@@ -4,19 +4,44 @@ from pyspark import StorageLevel
 from pyspark.ml import Transformer
 from pyspark.sql import SparkSession
 
-# Create Spark session
-spark = SparkSession.builder.appName("hw_03").getOrCreate()
-
-database = "baseball"
-user = "root"
-password = 1997
+# setting up database connection parameters
+database = input("Please enter database name: ")
+user = input("Please enter the user name: ")
+password = input("Please enter the password: ")
 server = "localhost"
 port = 3306
 jdbc_url = f"jdbc:mysql://{server}:{port}/{database}?permitMysqlScheme"
 jdbc_driver = "org.mariadb.jdbc.Driver"
 
 
-def get_mariadb_data(sql, temp_table_name):
+# creating a dummy class for overriding the transform method
+class customTransformer(Transformer):
+    def __init__(self, spark):
+        self.spark = spark
+
+    def _transform(self, df_game_batter_intm):
+        df_game_batter_intm.createOrReplaceTempView("game_batter_intm")
+        df_game_batter_intm.persist(StorageLevel.MEMORY_ONLY)
+
+        df_rolling_100_days_avg = self.spark.sql(
+            "select gb_1.batter,"
+            " gb_1.local_date, "
+            "coalesce( sum(gb_2.hit), 0) as cumalative_hits, "
+            "coalesce( sum(gb_2.atBat), 0) as cumalative_atBat,"
+            "round( if( sum(gb_2.atBat) = 0 or sum(gb_2.atBat) is null, 0, sum(gb_2.hit) / sum(gb_2.atBat)), 4) "
+            "as rolling_100_days_avg "
+            "from game_batter_intm gb_1 left join game_batter_intm gb_2 "
+            "on ( datediff(gb_1.local_date, gb_2.local_date) <= 100 and "
+            "datediff(gb_1.local_date, gb_2.local_date) > 0 "
+            "and gb_1.batter = gb_2.batter) "
+            "group by gb_1.batter, gb_1.local_date "
+            "order by gb_1.batter, gb_1.local_date "
+        )
+        return df_rolling_100_days_avg
+
+
+# function for getting data from the mariadb database
+def get_mariadb_data(sql, temp_table_name, spark):
     # Create a data frame by reading data from Oracle via JDBC
     df = (
         spark.read.format("jdbc")
@@ -32,8 +57,9 @@ def get_mariadb_data(sql, temp_table_name):
 
 
 def main():
-    get_mariadb_data("select * from batter_counts", "batter_counts")
-    get_mariadb_data("select * from game", "game")
+    spark = SparkSession.builder.appName("hw_03").getOrCreate()
+    get_mariadb_data("select * from batter_counts", "batter_counts", spark)
+    get_mariadb_data("select * from game", "game", spark)
 
     df_game_batter_intm = spark.sql(
         "select "
@@ -46,28 +72,8 @@ def main():
         "from batter_counts bt_C inner join game gm on (bt_C.game_id = gm.game_id)"
     )
 
-    class customTransformer(Transformer):
-        def _transform(self, game_batter_intm):
-            df_game_batter_intm.createOrReplaceTempView("game_batter_intm")
-            df_game_batter_intm.persist(StorageLevel.MEMORY_ONLY)
-
-            df_rolling_100_days_avg = spark.sql(
-                "select gb_1.batter,"
-                " gb_1.local_date, "
-                "coalesce( sum(gb_2.hit), 0) as cumalative_hits, "
-                "coalesce( sum(gb_2.atBat), 0) as cumalative_atBat,"
-                "round( if( sum(gb_2.atBat) = 0 or sum(gb_2.atBat) is null, 0, sum(gb_2.hit) / sum(gb_2.atBat)), 4) "
-                "as rolling_100_days_avg "
-                "from game_batter_intm gb_1 left join game_batter_intm gb_2 "
-                "on ( datediff(gb_1.local_date, gb_2.local_date) <= 100 and "
-                "datediff(gb_1.local_date, gb_2.local_date) > 0 "
-                "and gb_1.batter = gb_2.batter) "
-                "group by gb_1.batter, gb_1.local_date "
-                "order by gb_1.batter, gb_1.local_date "
-            )
-            return df_rolling_100_days_avg
-
-    cust_transofrmer = customTransformer()
+    # creating an object of the dummy class and overriding the transform method
+    cust_transofrmer = customTransformer(spark)
     df_rolling_100_days_avg = cust_transofrmer.transform(df_game_batter_intm)
 
     df_rolling_100_days_avg.createOrReplaceTempView("rolling_100_days_avg")
